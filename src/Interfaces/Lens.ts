3683f4bc0916be0ac6b4b336606383bdf8d79f58
unamednada <dias.gustavo.dias@gmail.com>
2022-04-08 12:04:14 -0300

    Fix lenschema casing and create lens service

--- src/Interfaces/Lens.ts
+++ src/Interfaces/Lens.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
-const lensSchema = z.object({
+const LensSchema = z.object({
   degree: z.number(),
   antiGlare: z.boolean(),
   blueLightFilter: z.boolean(),
 });
 
-type Lens = z.infer<typeof lensSchema>;
+type Lens = z.infer<typeof LensSchema>;
 
 export default Lens;
-export { lensSchema };
+export { LensSchema };
